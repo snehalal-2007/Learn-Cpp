352cc955b5d7a751abdbb0bfd4512e931b008089
Sneha Lal <snehalaltvm@gmail.com>
2026-01-09 17:48:55 -0600

    Unit 5

---
+++ 5_namespace.cpp
@@ -0,0 +1,30 @@
+//Prompt: 
+
+#include <iostream>
+
+namespace one { // this will create a namespace called 'one' and assigns x the value of 1
+    int x = 1;  
+}
+
+namespace two { // this will create a namespace called 'two' and assigns x the value of 2
+    int x = 2;  
+}
+
+int main(){
+    
+    int x = 0;
+    // int x = 1; // This will cause a compilation error since x is already declared in this scope
+
+    std::cout << "Value of x in global scope: " << x << std::endl; // prints the value of x in the global scope
+    std::cout << "Value of x in namespace one: " << one::x << std::endl; // prints the value of x in namespace one
+    std::cout << "Value of x in namespace two: " << two::x << std::endl; // prints the value of x in namespace two
+
+    return 0;
+}
+
+/*
+    Notes:
+        - Namespaces help organize code and prevent name conflicts.
+        - You can access variables in a namespace using the scope resolution operator (::), e.g., one::x or two::x.
+        - Variables declared in the same scope must have unique names. 
+*/
